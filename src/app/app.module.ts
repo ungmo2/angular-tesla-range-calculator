4945def70cde86998232eae79fcbccf393b4c9d8
leeungmo <ungmo2@gmail.com>
2017-04-13 00:11:27 +0900

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { TeslaBatteryModule } from './tesla-battery/tesla-battery.module';
 
 import { AppComponent } from './app.component';
 
@@ -11,8 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    TeslaBatteryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/tesla-battery/tesla-battery.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// services
+import { BatteryService } from './tesla-battery.service';
+// containers
+import { TeslaBatteryComponent } from './containers/tesla-battery/tesla-battery.component';
+import { TeslaCarComponent } from './components/tesla-car/tesla-car.component';
+import { TeslaStatsComponent } from './components/tesla-stats/tesla-stats.component';
+import { TeslaCounterComponent } from './components/tesla-counter/tesla-counter.component';
+import { TeslaClimateComponent } from './components/tesla-climate/tesla-climate.component';
+import { TeslaWheelsComponent } from './components/tesla-wheels/tesla-wheels.component';
+
+@NgModule({
+  declarations: [
+    // registering our container component
+    TeslaBatteryComponent,
+    TeslaCarComponent,
+    TeslaStatsComponent,
+    TeslaCounterComponent,
+    TeslaClimateComponent,
+    TeslaWheelsComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    // add the service to our sub-module
+    BatteryService
+  ],
+  exports: [
+    // exporting so our root module can access
+    TeslaBatteryComponent
+  ]
+})
+export class TeslaBatteryModule {}
